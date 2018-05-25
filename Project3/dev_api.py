eef206b0df04e4b69646c67c98584fcad2a8556e
Arthur <aroullier@l2tech.com>
2018-05-25 13:33:29 +0200

    script: start minimal script for the API

---
+++ Project3/dev_api.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 24 15:10:27 2018
+
+@author: arthur
+
+
+API for project 3 OC
+
+
+
+"""
+
+#%%
+
+
+from flask import Flask, jsonify, make_response
+
+
+
+#%% config
+
+entryPoint = 'http://[hostname]/P3/API/v0/filmID/(arg)'
+
+
+#%% views
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    
+    _str = 'Hello!<br \>Welcome to my API.<br \>Please use the following entry point:<br \><br \>     {}'.format(entryPoint)
+    return _str
+
+
+@app.route('/P3/API/v0/filmID/',methods=['GET'])
+def missingArgument():
+    return jsonify({'error': 'Missing argument'})
+
+
+@app.route('/P3/API/v0/filmID/<filmID>',methods=['GET']) # possible to force type with <int:arg>
+def get_twiceFilmID(filmID):
+    
+    if not filmID.isdigit():
+        res = jsonify({'error': 'Incorrect input type'})
+       
+    else:
+        filmID = int(filmID)
+        _twice = 2*filmID
+        res = jsonify({'twice': _twice})
+    
+    return res
+
+
+@app.errorhandler(404)
+def notFound(error):
+    return make_response(jsonify({'error': 'Not found'}),404)
+
+
+
+
+#%% app
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
