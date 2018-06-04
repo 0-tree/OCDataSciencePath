bc642e44dd9b433ca4a0c495516119cf91d3f699
Arthur <aroullier@l2tech.com>
2018-06-04 16:55:37 +0200

    model: ok first example of 5-recomendation based on actors triplet

--- Project3/App/dev_model.py
+++ Project3/App/dev_model.py
@@ -11,3 +11,77 @@ models for OC-DataScience-Project3
 
 """
 
+
+
+#%% imports
+
+import os
+HOME = os.path.expanduser('~/')
+HOST = os.uname()[1]
+if HOST == 'Arthurs-MacBook-Pro.local':
+    os.chdir(HOME+'/Documents/GitHub/OCDataSciencePath/Project3/')    # @home
+elif HOST == 'Sirius.local':
+    os.chdir(HOME+'Perso/GitHub/OCDataSciencePath/Project3/')         # @L2
+else:
+    raise ValueError('unknown host: {}'.format(HOST))
+    
+import numpy as np
+import pandas as pd
+
+from sklearn.neighbors import NearestNeighbors
+
+
+
+#%% load data
+
+if HOST == 'Arthurs-MacBook-Pro.local':
+    pathToData = HOME+'xxx'                                        # @home
+elif HOST == 'Sirius.local':
+    pathToData = HOME+'Downloads/movie_metadata_CLEAN.csv'         # @L2
+else:
+    raise ValueError('unknown host: {}'.format(HOST))
+    
+df = pd.read_csv(pathToData)
+
+
+#%% data (handy)
+
+X = df.iloc[:,4:].values
+print((X.sum(axis=1) != 3).sum())
+
+N = df['movie_title'].values
+A = df[['actor_1_name','actor_2_name','actor_3_name']].values
+
+n,p = X.shape
+
+
+
+#%% exampel of model (k-nn)
+
+nbReco = 5+1 # +1 because the calling film will always be at distance 0
+model = NearestNeighbors(n_neighbors=nbReco,
+                         algorithm='auto',
+                         metric='euclidean').fit(X)
+
+
+#%% example of recommendation call
+
+i0 = np.random.randint(n,size=1)[0]
+
+X0 = X[i0,:].reshape(1,-1)
+N0 = N[i0]
+
+d_nn,i_nn = model.kneighbors(X0)
+d_nn = d_nn[0,:]
+i_nn = i_nn[0,:]
+
+#print('closest to {}:\n{}'.format(i0,'\n'.join([str(i) for i in i_nn if i != i0])))
+print('closest to: {} (a: {})\n'.format(N0,A[i0,:]))
+for k,i in enumerate(i_nn):
+    if 0 < d_nn[k]:
+        print('{} (d={}) (a: {})'.format(N[i],np.round(d_nn[k],1),A[i,:]))
+
+
+
+
+
